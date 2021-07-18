77720bbb07301f66728defb2a9ef628f8d13b16a
王弈 <wangyi@wangyide-MacBook-Air.local>
2021-07-18 19:27:57 +0800

    ver.4 read docs before editing

--- products.py
+++ products.py
@@ -1,5 +1,30 @@
+# 增加新功能！(ver.4)
+# 讓使用者輸入前，先讀取檔案
+products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:# 在f中的每一行(line)，line是我自己取名的變數
+		name, price = line.strip().split(',') 
+		# 這邊當然可以寫成：
+			# s = line.strip().split(',')
+			# name = s[0]
+			# price = s[1]
+			# 但因為這個檔案他有兩個變數，所以我們可以直接取名字後，將剛剛分割出來的這兩塊，按順序([0], [1])分別存入name 跟 price
+		# 我們看到的檔案，他會換行，是因為每一行(line)裡面都包含換行符號'\n'，所以要先.strip()掉
+		# 因為line本身是字串，以目前的檔案來說他的第一行是「ramen,120」，
+		# 如果今天沒有用split來切的話，他在print出來的樣子就是 ramen,120 的字串
+		# 可是我今天讀取的目的就是要讓我能夠「單獨」「取出」個別資訊(ex.ramen)
+		# 我們學過什麼東西他是「可以一個包含多項資訊在內」的東西？
+		# 沒錯，就是list(清單) -> 所以我們就要用.split()把東西做分割
+		# 用.split()分割完之後就會儲存成list(清單)，以第一行來說「ramen,120」
+		# 他就讀取ramen跟120中間的','，並且從中間把它切開，再把兩者存回一個小清單當中[]，這樣就可以分別去讀取他們(ex. s[0])
+		# 用.spilt(',')來做分割 -> 意思是，在每一行(line)裡只要遇到','，就會被分割掉
+		# 所以換句話說，如果是用別的字串(ex. 'x')來分割，效果將有所不同
+		products.append([name, price])
+		# print(products) -> 這有什麼問題？ Ans:因為這個我是寫在for loop裡的，所以當我這個for loop跑了幾次，他就會印幾次
+print(products)
+
 # 我要建立一個清單，讓使用者可以持續輸入商品名稱
-products = [] # 使用者輸入完的商品要丟到清單裡，所以理當你應該開個清單讓消費者輸入的內容裝進去
+#products = [] # 使用者輸入完的商品要丟到清單裡，所以理當你應該開個清單讓消費者輸入的內容裝進去
 
 while True:
 	name = input('請輸入商品名稱： ')
@@ -9,7 +34,7 @@ while True:
 	if name == 'q':
 		break
 	# 可是只有商品沒有價格很怪耶，那我要建立一個二維清單怎麼做呢？
-	price = input('請輸入商品價過： ')
+	price = input('請輸入商品價格： ')
 	# p = [] # 建立一個小清單
 	# p.append(name) # 輸入的商品名稱裝進小清單[p]裡
 	# p.append(price) # 輸入的商品價格裝進小清單[p]裡
@@ -26,7 +51,7 @@ for p in products:
 # 'abc' * 3 = 'abcabcabc'
 
 # 接下來我要做的事情是，我要把這個清單裡的東西一個一個叫出來，並且存出去在電腦裡！
-with open('products.txt', 'w', encoding='utf-8') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
 	# open就是打開電腦檔案，電腦原先有沒有這個檔案不重要，有的話也會複寫過去
 	# 沒有的話也沒有關係，因為現在是寫入模式'w'，所以他會產生這個檔案
 	# as f 是什麼意思？複習一下就是「當作f」的意思
